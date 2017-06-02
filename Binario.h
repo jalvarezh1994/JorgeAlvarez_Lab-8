0b065a203b177dc8da5712282fbee0ac3e672649
jalvarezh1994 <j.alvarezh1994@unitec.edu>
2017-06-02 15:36:57 -0600

    Se terminaron todas las sobrecargas

--- Binario.h
+++ Binario.h
@@ -5,12 +5,66 @@ using namespace std;
 
 class Binario
 {
-	private:
-		int* Arreglo = new int[4];
-	public:
-		Binario();
-		~Binario();
-		void llenar();
-		int* getArreglo();
-		int* operator
+private:
+	int* Arreglo = new int[4];
+public:
+	Binario();
+	~Binario();
+	void llenar();
+	int* getArreglo();
+	int* operator +(Binario &b1){
+		int* resultado=new int[4];
+		for (int i = 0; i < 4; ++i){
+			if (b1.getArreglo()[i]+this->getArreglo()[i]>0){
+				resultado[i]=1;
+			}else{
+				resultado[i]=0;
+			}
+		}
+		return resultado;
+	}
+	int* operator -(Binario &b1){
+		int* resultado=new int[4];
+		for (int i = 0; i < 4; ++i){
+			if (b1.getArreglo()[i]+this->getArreglo()[i]==2){
+				resultado[i]=1;
+			}else{
+				resultado[i]=0;
+			}
+		}
+		return resultado;
+	}
+	int* operator *(Binario &b1){
+		int* resultado=new int[4];
+		for (int i = 0; i < 4; ++i){
+			if (this->getArreglo()[i]==1&&b1.getArreglo()[i]==0){
+				resultado[i]=0;
+			}else{
+				resultado[i]=1;
+			}
+		}
+		return resultado;
+	}
+	int* operator /(Binario &b1){
+		int* resultado=new int[4];
+		for (int i = 0; i < 4; ++i){
+			if (this->getArreglo()[i]+b1.getArreglo()[i]==1){
+				resultado[i]=0;
+			}else{
+				resultado[i]=1;
+			}
+		}
+		return resultado;
+	}
+	int* operator ^(Binario &b1){
+		int* resultado=new int[4];
+		for (int i = 0; i < 4; ++i){
+			if (this->getArreglo()[i]+b1.getArreglo()[i]==1){
+				resultado[i]=1;
+			}else{
+				resultado[i]=0;
+			}
+		}
+		return resultado;
+	}
 };
--- main.cpp
+++ main.cpp
@@ -15,10 +15,64 @@ int main(int argc, char const *argv[]){
 		cout<<"Número2: ";
 		imrpimirBinario(b2);
 		cout<<"Menú"<<endl;
+		cout<<"1. Disyunción(+)"<<endl;
+		cout<<"2. Conjunción(-)"<<endl;
+		cout<<"3. Implicación(*)"<<endl;
+		cout<<"4. Doble Implicación(/)"<<endl;
+		cout<<"5. Disyunción exclusiva(^)"<<endl;
+		cout<<"6. Agregar vector"<<endl;
+		cout<<"7. Salir"<<endl;
 		cout<<"Ingrese la opción que desea: ";
 		cin>>opcion;
+		switch(opcion){
+			case 1:{
+				int * resultado=new int[4];
+				resultado=*b1+*b2;
+				for (int i = 0; i < 4; ++i){
+					cout<<resultado[i]<<"\t";
+				}
+				cout<<endl;
+				break;
+			}
+			case 2:{
+				int * resultado=new int[4];
+				resultado=*b1-*b2;
+				for (int i = 0; i < 4; ++i){
+					cout<<resultado[i]<<"\t";
+				}
+				cout<<endl;
+				break;
+			}
+			case 3:{
+				int * resultado=new int[4];
+				resultado=(*b1)*(*b2);
+				for (int i = 0; i < 4; ++i){
+					cout<<resultado[i]<<"\t";
+				}
+				cout<<endl;
+				break;
+			}
+			case 4:{
+				int * resultado=new int[4];
+				resultado=(*b1)/(*b2);
+				for (int i = 0; i < 4; ++i){
+					cout<<resultado[i]<<"\t";
+				}
+				cout<<endl;
+				break;
+			}
+			case 5:{
+				int * resultado=new int[4];
+				resultado=(*b1)^(*b2);
+				for (int i = 0; i < 4; ++i){
+					cout<<resultado[i]<<"\t";
+				}
+				cout<<endl;
+				break;
+			}
+		}
 
-	} while (opcion!=5);
+	} while (opcion!=6);
 	return 0;
 }
 
